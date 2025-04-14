9d97161cce503592718011edb55bf5003f8bb08a
Eo-Jin Nam (Jake) <nams93627@naver.com>
2025-04-14 17:57:57 +0900

    250414-mainpage 완료 임박

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(function(){
+    // 헤더 햄버거 메뉴 표시
     $('header .burgerButton').click(function(){
         $(this).css({'backgroundColor':'var(--orangeF)'});
         if ($('header nav').css('display') == 'none') {
@@ -7,5 +8,28 @@ $(function(){
             $(this).css({'backgroundColor':'var(--blackC)','color':'var(--whitePerfect)'});
         }
         $('header nav').slideToggle();
+    });
+
+    // 댓글 창 이동 초기 설정
+    $('<li>' + $('#replies .repliesList>div>ul>li:last-of-type').html() + '</li>')
+        .prependTo('#replies .repliesList>div>ul');
+    $('#replies .repliesList>div>ul').css('marginLeft','-50%');
+
+    $('#replies .repliesList>p').click(function(){
+        // 다음 표시
+        if($(this).text() == '>'){
+            //alert($('#replies .repliesList>ul>li:last-of-type').html())
+            $('#replies .repliesList>div>ul').animate({'marginLeft':'-100%'} , 1000);
+        // 이전 표시    
+        } else {
+            $('#replies .repliesList>div>ul').animate({'marginLeft':'0'} , 1000, function(){
+                $('#replies .repliesList>div>ul>li:last-of-type').remove();
+            
+             $('<li>' + $('#replies .repliesList>div>ul>li:last-of-type').html() + '</li>')
+                .prependTo('#replies .repliesList>div>ul');
+                $('#replies .repliesList>div>ul').css('marginLeft','0%');
+            });
+            
+        }
     });
 });
