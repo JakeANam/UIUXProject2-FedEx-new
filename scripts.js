47f1a51f8ed162b391513bd74d5e491826f13e8b
Eo-Jin Nam (Jake) <nams93627@naver.com>
2025-04-15 17:58:21 +0900

    250415-메인 완료 임박

--- scripts.js
+++ scripts.js
@@ -11,23 +11,31 @@ $(function(){
     });
 
     // 댓글 창 이동 초기 설정
-    $('<li>' + $('#replies .repliesList>div>ul>li:last-of-type').html() + '</li>')
-        .prependTo('#replies .repliesList>div>ul');
-    $('#replies .repliesList>div>ul').css('marginLeft','-50%');
+    $('<li>' + $('#replies .replySlide>li:last').html() + '</li>')
+        .prependTo('#replies .replySlide');
+    $('#replies .replySlide').css('marginLeft','-50%');
 
     $('#replies .repliesList>p').click(function(){
         // 다음 표시
         if($(this).text() == '>'){
-            //alert($('#replies .repliesList>ul>li:last-of-type').html())
-            $('#replies .repliesList>div>ul').animate({'marginLeft':'-100%'} , 1000);
+            
+            $('#replies .replySlide').animate({'marginLeft':'-100%'} , 1000, function(){
+                
+                $('#replies .replySlide>li:first').remove();
+                $('<li>' + $('#replies .replySlide>li:first').html() + '</li>')
+                    .appendTo('#replies .replySlide');
+                $('#replies .replySlide').css('marginLeft','-50%');
+                
+                
+                
+            });
         // 이전 표시    
         } else {
-            $('#replies .repliesList>div>ul').animate({'marginLeft':'0'} , 1000, function(){
+            $('#replies .replySlide').animate({'marginLeft':'0'} , 1000, function(){
                 $('#replies .repliesList>div>ul>li:last-of-type').remove();
-            
-             $('<li>' + $('#replies .repliesList>div>ul>li:last-of-type').html() + '</li>')
-                .prependTo('#replies .repliesList>div>ul');
-                $('#replies .repliesList>div>ul').css('marginLeft','0%');
+                $('<li>' + $('#replies .replySlide>li:last').html() + '</li>')
+                    .prependTo('#replies .replySlide');
+                $('#replies .replySlide').css('marginLeft','-50%');
             });
             
         }
