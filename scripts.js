8675837d4e417cbb23072cdcf1454c1bd7a0a29f
Eo-Jin Nam (Jake) <nams93627@naver.com>
2025-04-30 17:31:59 +0900

    250430 - project 최종 완성

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@
 let toSlide = decideToSlice($(window).outerWidth()); // 표시할 배너 슬라이스 개수 결정
 setBusiListWidth(toSlide); // 배너 슬라이드의 너비 결정
 
-let originalWidth = $(window).outerWidth();
+let originalWidth = $(window).outerWidth(); // 처음 접속 시 화면 너비
 
 // jQuery 시행
-$(function(){
+$(function() {
     // 헤더 햄버거 메뉴 표시
-    $('header .burgerButton').click(function(){
+    $('header .burgerButton').click(function() {
         $(this).css({'backgroundColor':'var(--orangeF)'});
         if ($('header nav').css('display') == 'none') {
             $(this).css({'backgroundColor':'var(--orangeF)','color':'var(--blackC)'});
@@ -18,25 +18,23 @@ $(function(){
     });
 
     // 서브메뉴 표시
-    $('nav>ul>li>a').mouseenter(function(){
+    $('nav>ul>li>a').mouseenter(function() {
         $(this).siblings('ul').show();
     });
-    $('nav>ul>li').mouseleave(function(){
+    $('nav>ul>li').mouseleave(function() {
         $('nav>ul>li>ul').hide();
     });
 
     // 화면 크기가 바뀌면
-    $(window).resize(function(){
+    $(window).resize(function() {
         // 너비 1000px 이상인데 메인 메뉴 안보이게 했으면 보이게
         if (!$('nav').is(':visible') && $(window).outerWidth() >= 1000) {
             $('nav').css('display', 'block');
 
         // 1000px 미만인데 메인 메뉴 보이게 했으면 안보이게
         } else if ($('nav').is(':visible') && $(window).outerWidth() < 1000 && originalWidth >= 1000) {
-            
-                $('nav').css('display', 'none');
-                $('header .burgerButton').css({'backgroundColor':'var(--blackC)','color':'var(--whitePerfect)'});
-            
+            $('nav').css('display', 'none');
+            $('header .burgerButton').css({'backgroundColor':'var(--blackC)','color':'var(--whitePerfect)'});
         }
 
         // 비즈니스 리소스 셋업
@@ -48,12 +46,12 @@ $(function(){
     });
     
     // 비즈니스 리소스 왼쪽 이동
-    $('#business .businessSlide>p').click(function(){
+    $('#business .businessSlide>p').click(function() {
         // 다음 버튼
         if ($(this).text() == '>') {   
             $('#business .businessList').stop().animate({
                 'marginLeft': toSlide + '%'
-            }, 1000, function(){
+            }, 1000, function() {
                 $(this).children(':first').appendTo(this);
                 $(this).css('margin','0');
             }); 
@@ -69,23 +67,21 @@ $(function(){
     });
 
     // 메인 화면 화살표 회전
-    $('#blog .blogFirst a, #blog .blogList a, #direction .directionList a').hover(function(){
+    $('#blog .blogFirst a, #blog .blogList a, #direction .directionList a').hover(function() {
         $(this).children().css({'transform':'rotate(0deg)','transition':'0.5s'});
-    }, function(){
+    }, function() {
         $(this).children().css('transform', 'rotate(-45deg)');
     });
 
     // 온라인 사기경고 아코디언 메뉴
-    $("#blogpage .fraudWarning h2>span:last-child").click(function(){
+    $("#blogpage .fraudWarning h2>span:last-child").click(function() {
         if (!$(this).parent().next('p').is(':visible')) {
             $(this).css('transform','rotate(180deg)');
             $(this).parent().next('p').slideDown();
-
         } else {
             $(this).css('transform','rotate(0deg)');
             $(this).parent().next('p').slideUp();
         }
-        
     });
 });
 
